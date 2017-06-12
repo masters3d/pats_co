d5f7f7cbd77e5b9a133a9f95c55ff92cc54b84a5
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-12 14:38:02 -0700

    added generated literal objects

--- sales/js/app.js
+++ sales/js/app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+var store0 = {'name':'1st and Pike','minCustomer':23,'maxCustomer':65,'avgCookieSale':6.3};
+var store1 = {'name':'SeaTac Airport','minCustomer':3,'maxCustomer':24,'avgCookieSale':1.2};
+var store2 = {'name':'Seattle Center','minCustomer':11,'maxCustomer':38,'avgCookieSale':3.7};
+var store3 = {'name':'Capitol Hill','minCustomer':20,'maxCustomer':38,'avgCookieSale':2.3};
+var store4 = {'name':'Alki','minCustomer':2,'maxCustomer':16,'avgCookieSale':4.6};
