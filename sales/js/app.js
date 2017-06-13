116692cbb5bcd744320ff08b1e7f2cd4d65c3dc3
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-13 13:28:06 -0700

    moved object literal to constructors

--- sales/js/app.js
+++ sales/js/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
-var store0 = {'name':'1st and Pike','minCustomer':23,'maxCustomer':65,'avgCookieSale':6.3,'customersPerHour':customersPerHourRandom};
-var store1 = {'name':'SeaTac Airport','minCustomer':3,'maxCustomer':24,'avgCookieSale':1.2,'customersPerHour':customersPerHourRandom};
-var store2 = {'name':'Seattle Center','minCustomer':11,'maxCustomer':38,'avgCookieSale':3.7,'customersPerHour':customersPerHourRandom};
-var store3 = {'name':'Capitol Hill','minCustomer':20,'maxCustomer':38,'avgCookieSale':2.3,'customersPerHour':customersPerHourRandom};
-var store4 = {'name':'Alki','minCustomer':2,'maxCustomer':16,'avgCookieSale':4.6,'customersPerHour':customersPerHourRandom};
 
-function customersPerHourRandom(min, max) {
-  return Math.random() * (max - min) + min;
+var locationsInfo = [
+['1st and Pike' , 23 , 65 ,6.3],
+['SeaTac Airport', 3 ,24 , 1.2],
+['Seattle Center', 11, 38 , 3.7],
+['Capitol Hill', 20,38 , 2.3],
+['Alki', 2 ,16 , 4.6],
+];
+
+
+function Store (name, minCustomer, maxCustomer, avgCookieSale) {
+  this.name = name;
+  this.minCustomer = minCustomer;
+  this.maxCustomer = maxCustomer;
+  this.avgCookieSale = avgCookieSale;
 }
 
-var stores = [store0,store1,store2,store3,store4];
+Store.prototype.customersPerHour = function() {
+  return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+};
+
+var stores = [];
+
+for(each in locationsInfo){
+  var eachStore = new Store(
+          locationsInfo[each][0],
+          locationsInfo[each][1],
+          locationsInfo[each][2],
+          locationsInfo[each][3]
+        );
+  console.log(eachStore);
+  stores.push(eachStore);
+}
+
 var hours = [ '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 for (var i = 0; i < stores.length; i += 1){
@@ -18,9 +41,9 @@ for (var i = 0; i < stores.length; i += 1){
   var store = stores[i];
   var totalCookiesSold = 0;
   for (var j = 0; j < hours.length; j += 1){
-    var flooredNumber = Math.floor(store.avgCookieSale * store.customersPerHour(store.minCustomer, store.maxCustomer));
+    var flooredNumber = Math.floor(store.avgCookieSale * store.customersPerHour());
     // | Int Value of average sold | String description
-    locationsStats.push( [flooredNumber, hours[j] + ': ' + flooredNumber + ' cookies']);
+    locationsStats.push(flooredNumber);
     totalCookiesSold += flooredNumber;
   }
   console.log(locationsStats);
@@ -44,7 +67,7 @@ for (var each = 0; each < stores.length; each += 1){
 
   for (var eachList = 0; eachList < eachStore.locationsStats.length; eachList += 1){
     var li = document.createElement('li');
-    li.textContent = eachStore.locationsStats[eachList][1];
+    li.textContent = eachStore.locationsStats[eachList];
     ul.appendChild(li);
   }
   var liLast = document.createElement('li');
